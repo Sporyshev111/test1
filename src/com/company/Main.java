3148356dba936e0583483c17056774634fe41593
Sporyshev111 <savva.2004.02.27@gmail.com>
2022-02-13 14:37:23 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("D:\\work\\files\\logo.jpg"));
+            System.out.println(img);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
